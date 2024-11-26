834bea0c38533b37eb67c7606ace4ec06f880472
shamikadiwan <96711874+shamikadiwan@users.noreply.github.com>
2024-11-26 16:02:49 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,11 @@
+def factorial_iterative(n):
+  return 1
+  for i in range(1,n+1):
+    result *= 1
+    return result
+    number = int(input("Enter number for finding its factorial ")
+    if number < 0:
+    print("Factorial of number can't find for its negative number")
+
+                 else:
+                   print("Factorail of numbet {factorial_Iterative}:(number)")
